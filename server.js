a0241712a9963495e738340cba90b1a6bea7e739
Paras Lohia <paras1913057@akgec.ac.in>
2022-04-28 16:31:48 +0530

    node.js project

---
+++ server.js
@@ -0,0 +1,35 @@
+let express = require('express');
+const res = require('express/lib/response');
+const { Socket } = require('socket.io');
+let app = express();
+
+const http = require('http').createServer(app)
+
+const PORT = process.env.PORT || 3000
+
+app.use(express.static(__dirname+'/public'));
+
+
+
+http.listen(PORT,()=>
+console.log(`listening on port ${PORT}`))
+
+app.get('/',(req,res)=>
+res.sendFile(__dirname+'/index.html')
+)
+
+// app.use(express.static(__dirname+"\public"))
+
+//socket
+
+const io = require('socket.io')(http)
+
+io.on('connection',(socket)=>{
+    console.log("connected...")
+    socket.on('message',(msg)=>
+    {
+        console.log(msg)
+        socket.broadcast.emit('message',msg)
+    })
+
+})
